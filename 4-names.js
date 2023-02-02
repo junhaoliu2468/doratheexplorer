e7997c4094563b5b9b10c02c6eafd6b8feea41a6
junhaoliu2468 <junhaoliu2468@gmail.com>
2023-02-02 16:46:34 +0800

    trying out some stuff

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//local
+const secret = "Super Secret"
+//share
+const john = 'John'
+const peter = 'peter'
+
+console.log(secret)
+
+module.export(john,peter)
---
+++ 8-os-module.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+
+const user = os.userInfo()
+console.log(user)
+
+
+
+
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(),
+}
+
+console.log(currentOS)
